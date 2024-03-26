e6b692ea704713d061eda0a4e8b171205a1e6a2a
armadi1809 <azizrmadi@gmail.com>
2024-03-25 22:56:38 -0500

    Google auth primitives set up

--- go.mod
+++ go.mod
@@ -5,3 +5,10 @@ go 1.22.1
 require github.com/go-chi/chi/v5 v5.0.12
 
 require github.com/a-h/templ v0.2.598
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/nedpals/postgrest-go v0.1.3 // indirect
+	github.com/nedpals/supabase-go v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 
+	"github.com/armadi1809/moviesdiary/sb"
 	"github.com/armadi1809/moviesdiary/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/nedpals/supabase-go"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	sb.Init()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
@@ -23,9 +33,20 @@ func main() {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	})
+
+	r.Get("/login/google", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := sb.Client.Auth.SignInWithProvider(supabase.ProviderSignInOptions{
+			Provider:   "google",
+			RedirectTo: "http://localhost:3000/",
+		})
+		if err != nil {
+			fmt.Println("Error ocurred")
+		}
+		http.Redirect(w, r, resp.URL, http.StatusSeeOther)
+	})
 	slog.Info("Server Starting on Port 3000...")
 	r.Handle("/public/*", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
-	err := http.ListenAndServe(":3000", r)
+	err = http.ListenAndServe(":3000", r)
 
 	if err != nil {
 		fmt.Printf("An error occurred %v", err)
