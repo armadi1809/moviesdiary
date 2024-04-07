b0452f47843115bf9ca2a361440a0ff8d3a6bfa8
armadi1809 <azizrmadi@gmail.com>
2024-04-07 15:36:49 -0500

    Daisy ui setup complete

--- handlers/home.go
+++ handlers/home.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/armadi1809/moviesdiary/views"
@@ -9,6 +10,7 @@ import (
 func HomeHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user := getUserFromRequest(r)
+		fmt.Println("Heeeeereeee", user.Email)
 		component := views.Hello(user.Email)
 		err := component.Render(r.Context(), w)
 
--- handlers/middleware.go
+++ handlers/middleware.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/nedpals/supabase-go"
@@ -14,13 +15,17 @@ func WithAuth(sbClient *supabase.Client) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			cookie, err := r.Cookie("at")
+			fmt.Println("Heeeeeeereeee", cookie)
+
 			if err != nil {
-				http.Redirect(w, r, "/login", http.StatusPermanentRedirect)
+				http.Redirect(w, r, "/login/google", http.StatusPermanentRedirect)
+				return
 			}
 			accessToken := cookie.Value
 			user, err := sbClient.Auth.User(r.Context(), accessToken)
 			if err != nil {
-				http.Redirect(w, r, "/login", http.StatusPermanentRedirect)
+				http.Redirect(w, r, "/login/google", http.StatusPermanentRedirect)
+				return
 			}
 			ctx := context.WithValue(r.Context(), userInfoKey("userInfo"), *user)
 			next.ServeHTTP(w, r.WithContext(ctx))
