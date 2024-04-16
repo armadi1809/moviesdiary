f99d28b1b20a8935483001fa319d980f6db5675c
armadi1809 <azizrmadi@gmail.com>
2024-04-15 22:58:38 -0500

    initial styling of mymovies cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html", "./**/*.templ", "./**/*.go"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), Myclass],
 };
